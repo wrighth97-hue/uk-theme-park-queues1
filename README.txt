1a4489fccbab607bdafc1e42b72ce322b7f2db0f
wrighth97-hue <wrighth97@gmail.com>
2026-09-16 13:04:42 +0100

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,9 @@
+Optional park logo files go in this folder.
+Recommended filenames:
+alton-towers.png
+thorpe-park.png
+chessington.png
+legoland-windsor.png
+paultons-park.png
+blackpool-pleasure-beach.png
+flamingo-land.png
--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='uk-queues-v1';
+const CACHE='uk-queues-v5';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch',e=>{const u=new URL(e.request.url);if(u.hostname==='api.themeparks.wiki')return;e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>{const u=new URL(e.request.url);if(u.hostname==='api.themeparks.wiki')return;e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)))});
